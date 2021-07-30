40f67ccd7221ea87f0e8afbf09fb4e2984637495
Yukijudaii <820359253@qq.com>
2021-07-30 15:11:27 +0800

    2021-7-30
    171题，26进制数

---
+++ code/171.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Solution
+{
+public:
+    int sum = 0;
+    int index = 0;
+    int titleToNumber(string columnTitle)
+    {
+        for (int i = columnTitle.size() - 1; i >= 0; i--)
+        {
+            sum += (columnTitle[i] - 'A' + 1) * pow(26, index++);
+        }
+        return sum;
+    }
+};
